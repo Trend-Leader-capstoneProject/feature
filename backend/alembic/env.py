2454f29a051346c2651efd23eed3865528a0f263
DarkLight0418 <samseong0418@naver.com>
2026-07-21 12:57:11 +0900

    [env] compare_server_default 값 디폴트로 수정

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -43,7 +43,7 @@ def run_migrations_offline() -> None:
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
         compare_type=True,
-        compare_server_default=True,
+        compare_server_default=False,
         include_schemas=False,
     )
 
