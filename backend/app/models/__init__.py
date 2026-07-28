0bc929603aeb4b6db96606e956690679c8370724
DarkLight0418 <samseong0418@naver.com>
2026-07-28 11:57:37 +0900

    [feat] __init__ 추가

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.category import Category
 from app.models.oauth_account import OAuthAccount
 from app.models.trend import Trend
+from app.models.trend_category_map import TrendCategoryMap
 from app.models.user import User
 from app.models.user_interest_category import UserInterestCategory
 from app.models.user_profile import UserProfile
@@ -9,6 +10,7 @@ __all__ = [
     "Category",
     "OAuthAccount",
     "Trend",
+    "TrendCategoryMap",
     "User",
     "UserInterestCategory",
     "UserProfile",
