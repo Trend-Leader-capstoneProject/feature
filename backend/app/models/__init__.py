79fe504eed82910d0d1e37d817a49afbe8e60cac
DarkLight0418 <samseong0418@naver.com>
2026-07-28 09:42:07 +0900

    [feat] UserInterestCategory 추가

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,8 @@
 from app.models.category import Category
+from app.models.oauth_account import OAuthAccount
 from app.models.trend import Trend
 from app.models.user import User
-from app.models.oauth_account import OAuthAccount
+from app.models.user_interest_category import UserInterestCategory
 from app.models.user_profile import UserProfile
 
 __all__ = [
@@ -9,5 +10,6 @@ __all__ = [
     "OAuthAccount",
     "Trend",
     "User",
+    "UserInterestCategory",
     "UserProfile",
 ]
