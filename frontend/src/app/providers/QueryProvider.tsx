db9350c8fae5db0020691127c2b135a4ec8a4c01
DarkLight0418 <samseong0418@naver.com>
2026-08-04 08:57:27 +0900

    [feat-provider] 리액트 쿼리 프로바이더

---
+++ frontend/src/app/providers/QueryProvider.tsx
@@ -0,0 +1,26 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
+import { type PropsWithChildren, useState } from "react";
+
+export function QueryProvider({
+  children,  
+}: PropsWithChildren) {
+    const [queryCilent] = useState(
+        () => 
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            }),
+    );
+
+    return (
+        <QueryClientProvider client={queryCilent}>
+            {children}
+        </QueryClientProvider>
+    );
+}
