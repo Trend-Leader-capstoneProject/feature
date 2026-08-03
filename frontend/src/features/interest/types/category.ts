56949921f2486610b93ce14ec0aa7af47783e144
DarkLight0418 <samseong0418@naver.com>
2026-08-03 16:53:43 +0900

    [feat-type] 카테고리 타입

---
+++ frontend/src/features/interest/types/category.ts
@@ -0,0 +1,27 @@
+export type CategoryCode = 
+    | "FASHION"
+    | "FOOD"
+    | "IT_DIGITAL"
+    | "ENTERTAINMENT"
+    | "BEAUTY"
+    | "GAME";
+
+export interface CategoryItem {
+    category_id: number;
+    category_code: CategoryCode | null;   // 세부분류에서는 코드가 없기 때문에 null 가능
+    category_name: string;    
+    parent_id: number | null;
+    sort_order: number;
+    children: CategoryItem[];
+}
+
+export interface CategoryListData {
+    categories: CategoryItem[];
+}
+
+export interface CommonResponse<Data> {
+    success: true;
+    statusCode: number;
+    message: string;
+    data: Data;
+}
