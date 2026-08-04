c814619b6668ce93b67f451b7e3e00717533d60c
DarkLight0418 <samseong0418@naver.com>
2026-08-04 10:34:51 +0900

    [fix] 불필요한 prefix 제거

--- frontend/src/features/interest/api/getCategories.ts
+++ frontend/src/features/interest/api/getCategories.ts
@@ -5,7 +5,7 @@ import { CategoryListData, CommonResponse } from "../types/category";
 export async function getCategories(): Promise<CategoryListData> {
     const response = await apiRequest<
         CommonResponse<CategoryListData>
-    >("/api/categories");
+    >("/categories");
 
     return response.data;
 }
