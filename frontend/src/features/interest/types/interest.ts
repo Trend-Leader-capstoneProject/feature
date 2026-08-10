77e22aef412e1a35b4a685d1d8763b6d0c89c330
DarkLight0418 <samseong0418@naver.com>
2026-08-10 10:38:00 +0900

    [feat/frontend] 관심사 오류 타입 추가

--- frontend/src/features/interest/types/interest.ts
+++ frontend/src/features/interest/types/interest.ts
@@ -1,3 +1,5 @@
+import { CommonErrorResponse, ValidationErrorData } from "../../../shared/types/api";
+
 export interface InterestSaveRequest {
     category_ids: number[];
 }
@@ -7,3 +9,28 @@ export interface InterestSaveResponse {
     selected_count: number;
 }
 
+export interface InterestCategoryRuleErrorData {
+  inactive_category_ids: number[];
+  child_category_ids: number[];
+}
+
+export interface InterestMissingCategoryErrorData {
+  category_ids: number[];
+}
+
+export type InterestSaveErrorResponse =
+  | CommonErrorResponse<
+      InterestCategoryRuleErrorData,
+      400
+    >
+  | CommonErrorResponse<null, 401>
+  | CommonErrorResponse<
+      InterestMissingCategoryErrorData,
+      404
+    >
+  | CommonErrorResponse<null, 409>
+  | CommonErrorResponse<
+      ValidationErrorData,
+      422
+    >
+  | CommonErrorResponse<null, 500>;
