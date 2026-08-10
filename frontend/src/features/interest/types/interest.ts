402731e01fba6a9b16e2ab6d41b45b419e2c9254
DarkLight0418 <samseong0418@naver.com>
2026-08-10 09:41:09 +0900

    [feat/frontend] 관심사 타입 작업

---
+++ frontend/src/features/interest/types/interest.ts
@@ -0,0 +1,9 @@
+export interface InterestSaveRequest {
+    category_ids: number[];
+}
+
+export interface InterestSaveResponse {
+    selected_category_ids: number[];
+    selected_count: number;
+}
+
