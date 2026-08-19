3b33da76050d025fe5ab7e007e9238d577748f9a
DarkLight0418 <samseong0418@naver.com>
2026-08-19 14:45:19 +0900

    [fix/app] 엔트리 구조 변경

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,14 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { RootNavigator } from "./src/app/navigation/RootNavigator";
+import { AuthProvider } from "./src/app/providers/AuthProvider";
 import { QueryProvider } from "./src/app/providers/QueryProvider";
 
 export default function AppEntry() {
   return (
     <SafeAreaProvider>
       <QueryProvider>
-        <StatusBar style="dark" />
-        <RootNavigator />
+        <AuthProvider>
+          <StatusBar style="dark" />
+          <RootNavigator />
+        </AuthProvider>
       </QueryProvider>
     </SafeAreaProvider>
   );
