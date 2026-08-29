e9b80e518d940907e9c76302b2091b0b4c266ec5
DarkLight0418 <samseong0418@naver.com>
2026-08-29 21:01:01 +0900

    [edit] 코드 컨벤션 스타일 정리

--- frontend/src/features/auth/hooks/useSignup.ts
+++ frontend/src/features/auth/hooks/useSignup.ts
@@ -1,7 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
-import { signup } from "../api/signup";
-import { SignupErrorResponse, SignupRequest, SignupResponse } from "../types/auth";
+import {
+    useMutation,
+} from "@tanstack/react-query";
+import type {
+    AxiosError,
+} from "axios";
+
+import {
+    signup,
+} from "../api/signup";
+import type {
+    SignupErrorResponse,
+    SignupRequest,
+    SignupResponse,
+} from "../types/auth";
 
 type SignupSuccessHandler = (
   result: SignupResponse,
@@ -10,13 +21,13 @@ type SignupSuccessHandler = (
 export function useSignup(
   onSuccess?: SignupSuccessHandler,
 ) {
-    return useMutation<
-      SignupResponse,
-      AxiosError<SignupErrorResponse>,
-      SignupRequest
-    >({
-        mutationFn: signup,
-        onSuccess,
-        retry: 0,
-    })
+  return useMutation<
+    SignupResponse,
+    AxiosError<SignupErrorResponse>,
+    SignupRequest
+  >({
+    mutationFn: signup,
+    onSuccess,
+    retry: 0,
+  });
 }
